9e0aa59890e3a2f0882f9d1b9558ae41da34c110
kkr95101 <kkr95101@naver.com>
2023-07-20 14:49:20 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/java-project-team1-aaaa.git

--- src/com/itwill/shop/test/CartDaoTestMain.java
+++ src/com/itwill/shop/test/CartDaoTestMain.java
@@ -37,14 +37,4 @@ public class CartDaoTestMain {
 		//System.out.println(">>"+cartDao.findSelectAll("user1"));
 	}
 	
-	public static void main(String[] args)throws Exception{
-		CartDao cartDao = new CartDao();
-		
-		//1 카트추가
-		System.out.println("카트추가>> "+cartDao.insert(new Cart(0, 3, "aa", new Product(1,"문과 남자의 과학공부", 15750, "/com/itwill/shop/image/bestSeller1", "유시민이 들려주는 과학 인문학이야기"))));
-		
-		//2 id, 상품종류로 카트제품존재(개수)확인
-		System.out.println("카트개수>>"+cartDao.countByCart("sy1", 3));
-		
-	}
 }
