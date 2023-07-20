d57c8488e06c0f61d647dc3eefde028e596a9360
kkr95101 <kkr95101@naver.com>
2023-07-20 14:48:37 +0900

    afd

--- src/com/itwill/shop/order/OrderDao.java
+++ src/com/itwill/shop/order/OrderDao.java
@@ -13,4 +13,16 @@ public class OrderDao {
 		dataSource = new DataSource();
 	}
 	
+	public int deleteByMemberId(String m_id)throws Exception{
+		Connection con =dataSource.getConnection();
+		PreparedStatement pstmt =con.prepareStatement(OrderSQL.ORDER_DELETE_BY_M_ID);
+		pstmt.setString(1, m_id);
+		int rowCount = 0;
+		rowCount = pstmt.executeUpdate();
+			con.close();
+			pstmt.close();
+		
+		return 0;
+	}
+	
 }
