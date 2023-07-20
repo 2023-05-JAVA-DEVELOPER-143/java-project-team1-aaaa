7b1e4c39deea203fd210b138d6566173d7dc2a54
leedong617 <leedong617@gmail.com>
2023-07-20 10:07:34 +0900

    product_insert.sql 수정

--- 산출물/02.테이블정의서(ERD)/product_insert.sql
+++ 산출물/02.테이블정의서(ERD)/product_insert.sql
@@ -1,7 +1,7 @@
 insert into product(p_no,p_name,p_price,p_image,p_desc) values(product_p_no_SEQ.nextval, '문과 남자의 과학공부', 15750, '/com/itwill/shop/image/bestSeller1', '유시민이 들려주는 과학 인문학이야기');
 insert into product(p_no,p_name,p_price,p_image,p_desc) values(product_p_no_SEQ.nextval, '스킵과 로퍼 6', 5400, '/com/itwill/shop/image/bestSeller2', '도쿄 고교생활 속 연애이야기');
 insert into product(p_no,p_name,p_price,p_image,p_desc) values(product_p_no_SEQ.nextval, '도둑맞은 집중력', 16920, '/com/itwill/shop/image/bestSeller3', '삶에서 도난당하고 있는 집중력을 되찾는법을 담은책');
-insert into product(p_no,p_name,p_price,p_image,p_desc) values(product_p_no_SEQ.nextval, '스튜디오 지브리 입체건축전 도록 (복각판)', 27000, '/com/itwill/shop/image/bestSeller4', '스튜디오 지브리 입체건축전 전시회 도록');
+insert into product(p_no,p_name,p_price,p_image,p_desc) values(product_p_no_SEQ.nextval, '스튜디오 지브리 입체건축전 도록', 27000, '/com/itwill/shop/image/bestSeller4', '스튜디오 지브리 입체건축전 전시회 도록');
 insert into product(p_no,p_name,p_price,p_image,p_desc) values(product_p_no_SEQ.nextval, '세이노의가르침', 6480, '/com/itwill/shop/image/selfImprovement1', '순자산 천억원대 자산가의 인생이야기');
 insert into product(p_no,p_name,p_price,p_image,p_desc) values(product_p_no_SEQ.nextval, '최적의공부뇌', 15120, '/com/itwill/shop/image/selfImprovement2', '공부를 잘하기 위한 최소조건인 뇌의최적화에 대해');
 insert into product(p_no,p_name,p_price,p_image,p_desc) values(product_p_no_SEQ.nextval, '역행자 확장판', 17550, '/com/itwill/shop/image/selfImprovement3', '운명과 본능의 지배에서 벗어나 경제적 자유와 행복을 쟁취하는 라이프해킹의 비밀을 담은 책');
