fc40adc2c5de26f065e342dabef0912fd8756a9b
배종호 <ehtmek9@gmail.com>
2023-07-24 10:29:02 +0900

    ..

--- src/com/itwill/shop/ui/배종호/MainCategoryPanel.java
+++ src/com/itwill/shop/ui/배종호/MainCategoryPanel.java
@@ -1,6 +1,7 @@
 package com.itwill.shop.ui.배종호;
 import java.awt.BorderLayout;
 import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -13,37 +14,40 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import com.itwill.shop.test.Main2;
+import java.awt.GridLayout;
+import java.awt.Color;
+import javax.swing.JScrollPane;
 
 public class MainCategoryPanel extends JPanel {
-		
-
-	/**
-	 * Create the panel.
-	 */
-	private Main2 mainFrame;
-		
-	public MainCategoryPanel() {
 	
-		setLayout(new BorderLayout(0, 0));
-			
+	private Main2 mainFrame;
+	
+	
+	
+		public MainCategoryPanel() {
+		 setPreferredSize(new Dimension(460, 500));  //크기설정
+	        setLayout(new BorderLayout(0, 0));
+	        
+	       
 		JPanel categoryPane = new JPanel();
 		add(categoryPane, BorderLayout.CENTER);
 		categoryPane.setLayout(null);
+		
 			
 		JLabel bestSellerLabel = new JLabel("");
 		bestSellerLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		bestSellerLabel.addMouseListener(new MouseAdapter() {
 			@Override
 				public void mouseClicked(MouseEvent e) {
-					/***************베스트셀러화면패널보여주세요***********************/
-					System.out.println("베스트셀러화면패널보여주세요");
+					
 					mainFrame.changePanel(2, 0,-1,null);
 					
 				}
+			
 		});
 		bestSellerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		bestSellerLabel.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/bestSeller1.jpg")));
-		bestSellerLabel.setBounds(31, 52, 125, 185);
+		bestSellerLabel.setIcon(null);
+		bestSellerLabel.setBounds(10, 32, 423, 182);
 		categoryPane.add(bestSellerLabel);
 			
 		JLabel nonFictionLabel = new JLabel("");
@@ -51,14 +55,13 @@ public class MainCategoryPanel extends JPanel {
 		nonFictionLabel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-					/***************소설화면패널보여주세요***********************/
-					System.out.println("소설화면패널보여주세요");
+					
 					mainFrame.changePanel(2, 1,-1,null);
 				}
 			});
-			nonFictionLabel.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/bestSeller2.jpg")));
+			nonFictionLabel.setIcon(null);
 			nonFictionLabel.setHorizontalAlignment(SwingConstants.CENTER);
-			nonFictionLabel.setBounds(168, 52, 125, 185);
+			nonFictionLabel.setBounds(12, 302, 421, 160);
 			categoryPane.add(nonFictionLabel);
 			
 			JLabel localBookLabel = new JLabel("");
@@ -66,49 +69,29 @@ public class MainCategoryPanel extends JPanel {
 			localBookLabel.addMouseListener(new MouseAdapter() {
 				@Override
 				public void mouseClicked(MouseEvent e) {
-					/***************국내도서화면패널보여주세요***********************/
-					System.out.println("국내도서화면패널보여주세요");
+					
 					mainFrame.changePanel(2, 2,-1,null);
 				}
 			});
-			localBookLabel.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/bestSeller3.jpg")));
+			localBookLabel.setIcon(null);
 			localBookLabel.setHorizontalAlignment(SwingConstants.CENTER);
-			localBookLabel.setBounds(305, 52, 125, 185);
+			localBookLabel.setBounds(0, 511, 415, 120);
+			categoryPane.add(localBookLabel);
 			categoryPane.add(localBookLabel);
 			
-			JButton bestSellerButton = new JButton("베스트셀러");
+			JLabel backGroud = new JLabel(""); 
+			backGroud.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/mainbook.jpg"))); 
+			backGroud.setHorizontalAlignment(SwingConstants.CENTER);
+			backGroud.setBounds(10, 41, 460, 500);
+			categoryPane.add(backGroud);
 			
-			
-			bestSellerButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					/***************베스트셀러화면패널보여주세요***********************/
-					System.out.println("베스트셀러화면패널보여주세요");
-					
-				}
-			});
-			bestSellerButton.setBounds(41, 247, 97, 23);
-			categoryPane.add(bestSellerButton);
-			
-			JButton nonFictonButton = new JButton("소설");
-			nonFictonButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					/***************소설화면패널보여주세요***********************/
-					System.out.println("소설화면패널보여주세요");
-				}
-			});
-			nonFictonButton.setBounds(178, 247, 97, 23);
-			categoryPane.add(nonFictonButton);
-			
-			JButton localBookButton = new JButton("국내도서");
-			localBookButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					/***************국내도서화면패널보여주세요***********************/
-					System.out.println("국내도서화면패널보여주세요");
-				}
-			});
-			localBookButton.setBounds(315, 247, 97, 23);
-			categoryPane.add(localBookButton);
+			JScrollPane scrollPane = new JScrollPane(backGroud);
+			scrollPane.setBounds(0, 0, 460, 700);
+			categoryPane.add(scrollPane);
+		
 		}
+		
+		
 	
 
 		public void setMainFrame(Main2 mainFrame) {
