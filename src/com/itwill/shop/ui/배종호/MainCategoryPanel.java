fc0a6abd534151f3c6f0989b592e3a65e7073f30
배종호 <ehtmek9@gmail.com>
2023-07-24 17:04:34 +0900

    ..

--- src/com/itwill/shop/ui/배종호/MainCategoryPanel.java
+++ src/com/itwill/shop/ui/배종호/MainCategoryPanel.java
@@ -2,11 +2,15 @@ package com.itwill.shop.ui.배종호;
 import java.awt.BorderLayout;
 import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -15,88 +19,140 @@ import javax.swing.SwingConstants;
 
 import com.itwill.shop.test.Main2;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Color;
 import javax.swing.JScrollPane;
+import java.awt.Rectangle;
+import javax.swing.border.LineBorder;
 
 public class MainCategoryPanel extends JPanel {
 	
 	private Main2 mainFrame;
 	
-	
-	
-		public MainCategoryPanel() {
-		 setPreferredSize(new Dimension(460, 500));  //크기설정
-	        setLayout(new BorderLayout(0, 0));
-	        
-	       
-		JPanel categoryPane = new JPanel();
-		add(categoryPane, BorderLayout.CENTER);
-		categoryPane.setLayout(null);
+	JScrollPane scrollPane = new JScrollPane();
+		public MainCategoryPanel() throws Exception{
+			setPreferredSize(new Dimension(500, 572));
+		setBackground(Color.LIGHT_GRAY);
+		setLayout(null);
 		
-			
-		JLabel bestSellerLabel = new JLabel("");
-		bestSellerLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		bestSellerLabel.addMouseListener(new MouseAdapter() {
-			@Override
-				public void mouseClicked(MouseEvent e) {
-					
-					mainFrame.changePanel(2, 0,-1,null);
-					
-				}
-			
-		});
-		bestSellerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		bestSellerLabel.setIcon(null);
-		bestSellerLabel.setBounds(10, 32, 423, 182);
-		categoryPane.add(bestSellerLabel);
-			
-		JLabel nonFictionLabel = new JLabel("");
-		nonFictionLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		nonFictionLabel.addMouseListener(new MouseAdapter() {
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setPreferredSize(new Dimension(2, 480));
+		//scrollPane.setPreferredSize(new Dimension(460, 500));
+		scrollPane.setBackground(Color.WHITE);
+		scrollPane.setBounds(0, 0, 470, 572);
+		add(scrollPane);
+		
+		JPanel backgroundPanel = new JPanel();
+		backgroundPanel.setBackground(Color.WHITE);
+		
+		
+		
+		backgroundPanel.setPreferredSize(new Dimension(10, 800));
+		scrollPane.setViewportView(backgroundPanel);
+		backgroundPanel.setLayout(null);
+		
+		JPanel bestPanel = new JPanel();
+		bestPanel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		bestPanel.setBackground(Color.WHITE);
+		bestPanel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-					
-					mainFrame.changePanel(2, 1,-1,null);
-				}
-			});
-			nonFictionLabel.setIcon(null);
-			nonFictionLabel.setHorizontalAlignment(SwingConstants.CENTER);
-			nonFictionLabel.setBounds(12, 302, 421, 160);
-			categoryPane.add(nonFictionLabel);
-			
-			JLabel localBookLabel = new JLabel("");
-			localBookLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-			localBookLabel.addMouseListener(new MouseAdapter() {
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					
-					mainFrame.changePanel(2, 2,-1,null);
-				}
-			});
-			localBookLabel.setIcon(null);
-			localBookLabel.setHorizontalAlignment(SwingConstants.CENTER);
-			localBookLabel.setBounds(0, 511, 415, 120);
-			categoryPane.add(localBookLabel);
-			categoryPane.add(localBookLabel);
-			
-			JLabel backGroud = new JLabel(""); 
-			backGroud.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/mainbook.jpg"))); 
-			backGroud.setHorizontalAlignment(SwingConstants.CENTER);
-			backGroud.setBounds(10, 41, 460, 500);
-			categoryPane.add(backGroud);
-			
-			JScrollPane scrollPane = new JScrollPane(backGroud);
-			scrollPane.setBounds(0, 0, 460, 600);
-			categoryPane.add(scrollPane);
+				mainFrame.changePanel(2,0, -1, null);
+			}
+		});
+		bestPanel.setBounds(0, 0, 469, 196);
+		backgroundPanel.add(bestPanel);
+		bestPanel.setLayout(null);
 		
-		}
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/bestSeller1.jpg")));
+		lblNewLabel.setBounds(25, 10, 118, 176);
+		bestPanel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_1.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/bestSeller2.jpg")));
+		lblNewLabel_1.setBounds(168, 10, 118, 176);
+		bestPanel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_2.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/bestSeller3.jpg")));
+		lblNewLabel_2.setBounds(311, 10, 118, 176);
+		bestPanel.add(lblNewLabel_2);
+		
+		JPanel novelPanel = new JPanel();
+		novelPanel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		novelPanel.setBackground(Color.WHITE);
+		novelPanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				mainFrame.changePanel(2,1, -1, null);
+			}
+		});
+		novelPanel.setBounds(0, 249, 469, 196);
+		backgroundPanel.add(novelPanel);
+		novelPanel.setLayout(null);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_3.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/novel1.jpg")));
+		lblNewLabel_3.setBounds(25, 10, 118, 176);
+		novelPanel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("");
+		lblNewLabel_1_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_1_1.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/novel2.jpg")));
+		lblNewLabel_1_1.setBounds(169, 10, 118, 176);
+		novelPanel.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("");
+		lblNewLabel_2_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_2_1.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/novel3.jpg")));
+		lblNewLabel_2_1.setBounds(316, 10, 118, 176);
+		novelPanel.add(lblNewLabel_2_1);
+		
+		JPanel localPanel = new JPanel();
+		localPanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				mainFrame.changePanel(2,2, -1, null);
+			}
+		});
+		localPanel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		localPanel.setBackground(Color.WHITE);
+		localPanel.setBounds(0, 498, 469, 196);
+		backgroundPanel.add(localPanel);
+		localPanel.setLayout(null);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("");
+		lblNewLabel_3_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_3_1.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/selfImprovement1.jpg")));
+		lblNewLabel_3_1.setBounds(25, 10, 118, 176);
+		localPanel.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("");
+		lblNewLabel_1_1_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_1_1_1.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/selfImprovement2.jpg")));
+		lblNewLabel_1_1_1.setBounds(168, 10, 118, 176);
+		localPanel.add(lblNewLabel_1_1_1);
+		
+		JLabel lblNewLabel_2_1_1 = new JLabel("");
+		lblNewLabel_2_1_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_2_1_1.setIcon(new ImageIcon(MainCategoryPanel.class.getResource("/com/itwill/shop/image/selfImprovement3.jpg")));
+		lblNewLabel_2_1_1.setBounds(322, 10, 118, 176);
+		localPanel.add(lblNewLabel_2_1_1);
 		
 		
-	
-
-		public void setMainFrame(Main2 mainFrame) {
-			this.mainFrame=mainFrame;
-			
-		}
+		
+		
 	}
+		public void setMainFrame(Main2 main2) {
+			this.mainFrame=main2;
+			
+		}
+}
+
+
 
